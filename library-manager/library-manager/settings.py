9e0ea0025a5bfbc0a67dc00e0239f687464cde5f
Huzaifa785 <huzaifa.coder785@gmail.com>
2020-11-21 16:27:00 +0530

    Removed SMTP Settings

--- library-manager/library-manager/settings.py
+++ library-manager/library-manager/settings.py
@@ -130,12 +130,3 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
-#SMTP Configuration
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'huzaifa.coder785@gmail.com'
-EMAIL_HOST_PASSWORD = 'MDHuz@if@7B5'
