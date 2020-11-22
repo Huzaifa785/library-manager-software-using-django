b569f18b06a75ed0a4a08ca16c95885799e3ab16
Huzaifa785 <huzaifa.coder785@gmail.com>
2020-11-22 12:39:09 +0530

    Improved some of the stuff

--- library-manager/library-manager/urls.py
+++ library-manager/library-manager/urls.py
@@ -18,14 +18,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('accounts.urls')),
     path('',views.index,name="Index"),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
